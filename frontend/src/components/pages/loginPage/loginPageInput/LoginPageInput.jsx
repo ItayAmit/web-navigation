2b21ef520f8eb1b761873bab70541ac3d3a9c531
ItayAmit <itayamit123@gmail.com>
2022-01-22 13:10:11 +0200

    Now getting type via props

--- frontend/src/components/pages/loginPage/loginPageInput/LoginPageInput.jsx
+++ frontend/src/components/pages/loginPage/loginPageInput/LoginPageInput.jsx
@@ -1,19 +1,19 @@
 import './loginPageInput.css';
 
-export function LoginPageInput({ title, value, onChange }) {
-  const onInputChanged = event => {
-    onChange(event.target.value);
-  };
+export function LoginPageInput({ type, title, value, onChange }) {
+    const onInputChanged = event => {
+        onChange(event.target.value);
+    };
 
-  return (
-    <div className="login-page-input-container">
-      <span className="login-page-input-title">{title}</span>
-      <input
-        type="text"
-        className="login-page-input"
-        value={value}
-        onChange={onInputChanged}
-      />
-    </div>
-  );
+    return (
+        <div className="login-page-input-container">
+            <span className="login-page-input-title">{title}</span>
+            <input
+                type={type}
+                className="login-page-input"
+                value={value}
+                onChange={onInputChanged}
+            />
+        </div>
+    );
 }
