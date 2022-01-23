d0f0da0616f9d75e14c37514aa07423be798df57
ItayAmit <itayamit123@gmail.com>
2022-01-24 00:54:34 +0200

    Cors added

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,7 +1,10 @@
 const express = require('express');
+const cors = require('cors');
 const dotenv = require('dotenv');
 dotenv.config({ path: './.env' });
 const app = express();
+
+app.use(cors());
 
 const port = process.env.PORT || 5000;
 
