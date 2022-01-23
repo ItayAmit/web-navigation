3319a33305a9bb991290fe3da5ac3e254ab72277
ItayAmit <itayamit123@gmail.com>
2022-01-24 00:53:42 +0200

    React router implemented

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,24 @@
 import React from 'react';
+import Axios from 'axios';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 
 import { LoginPage } from './components/pages/loginPage';
+import { RegisterPage } from './components/pages/registerPage';
 
 function App() {
     return (
-        <div>
-            <LoginPage />
-        </div>
+        <Router>
+            <>
+                <Switch>
+                    <Route path="/register">
+                        <RegisterPage />
+                    </Route>
+                    <Route path="/login">
+                        <LoginPage />
+                    </Route>
+                </Switch>
+            </>
+        </Router>
     );
 }
 
