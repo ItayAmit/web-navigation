f0dfe95b3f89d427f3acf2d6adf68a37caf5f96e
ItayAmit <itayamit123@gmail.com>
2022-01-25 18:05:58 +0200

    Removed the use of axios from App

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import Axios from 'axios';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 
 import { LoginPage } from './components/pages/loginPage';
