8a1efa8374e13a436b868b21072492619fce26b8
ItayAmit <itayamit123@gmail.com>
2022-01-25 18:08:56 +0200

    Added base for handling http requets

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,12 +1,38 @@
 const express = require('express');
 const cors = require('cors');
+const httpStatus = require('http-status');
 const dotenv = require('dotenv');
+const User = require('./database');
+
 dotenv.config({ path: './.env' });
 const app = express();
 
 app.use(cors());
+app.use(express.json());
 
 const port = process.env.PORT || 5000;
+
+app.post('/register', async (req, res) => {
+    const { username, password } = req.body;
+    const user = new User({ username, password });
+    if (await User.exists({ username })) {
+        res.sendStatus(httpStatus.BAD_REQUEST);
+    } else {
+        await user.save();
+        console.log('User has been saved');
+        res.sendStatus(httpStatus.OK);
+    }
+});
+
+app.post('/login', (req, res) => {
+    const { username, password } = req.body;
+    const user = new User({ username, password });
+    if (await User.exists({ username })) {
+        res.sendStatus(httpStatus.OK);
+    } else {
+        res.sendStatus(httpStatus.BAD_REQUEST);
+    }
+});
 
 app.listen(port, () => {
     console.log(`Listening on port ${port}`);
