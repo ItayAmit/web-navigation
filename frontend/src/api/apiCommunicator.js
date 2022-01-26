dc555a7d7398e0c43e5bdd052e788363c9552edf
ItayAmit <itayamit123@gmail.com>
2022-01-26 18:24:09 +0200

    Logged response from server

--- frontend/src/api/apiCommunicator.js
+++ frontend/src/api/apiCommunicator.js
@@ -8,9 +8,13 @@ export const apiCommunicator = {
 const API_URL = 'http://localhost:5000';
 
 function get(path) {
-    return Axios.get(`${API_URL}/${path}`);
+    return Axios.get(`${API_URL}/${path}`)
+        .then(response => console.log(response))
+        .catch();
 }
 
 function post(path, body) {
-    return Axios.post(`${API_URL}/${path}`, body);
+    return Axios.post(`${API_URL}/${path}`, body)
+        .then(response => console.log(response))
+        .catch();
 }
