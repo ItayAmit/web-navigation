d5788dbfd57bf7c91de6635c84f98bcbc76140f1
ItayAmit <itayamit123@gmail.com>
2022-01-26 18:06:36 +0200

    Hash and salt integrated

--- backend/src/index.js
+++ backend/src/index.js
@@ -4,6 +4,9 @@ const httpStatus = require('http-status');
 const dotenv = require('dotenv');
 const User = require('./database');
 
+const bcrypt = require('bcrypt');
+const saltRounds = 10;
+
 dotenv.config({ path: './.env' });
 const app = express();
 
@@ -12,24 +15,35 @@ app.use(express.json());
 
 const port = process.env.PORT || 5000;
 
-app.post('/register', async (req, res) => {
+app.post('/register', (req, res) => {
     const { username, password } = req.body;
-    const user = new User({ username, password });
-    if (await User.exists({ username })) {
-        res.sendStatus(httpStatus.BAD_REQUEST);
-    } else {
-        await user.save();
-        console.log('User has been saved');
-        res.sendStatus(httpStatus.OK);
-    }
+    bcrypt.hash(password, saltRounds, async function (err, hash) {
+        const user = new User({ username, password: hash });
+        if (await User.exists({ username })) {
+            console.log(`User with the username ${username} already exists`);
+            res.sendStatus(httpStatus.BAD_REQUEST);
+        } else {
+            await user.save();
+            console.log('User has been saved');
+            res.sendStatus(httpStatus.OK);
+        }
+    });
 });
 
 app.post('/login', async (req, res) => {
     const { username, password } = req.body;
-    const user = new User({ username, password });
     if (await User.exists({ username })) {
-        res.sendStatus(httpStatus.OK);
+        const user = await User.findOne({ username });
+        bcrypt.compare(password, user.password, function (err, result) {
+            if (result) {
+                console.log(`User ${username} has logged in`);
+                res.sendStatus(httpStatus.OK);
+            } else {
+                console.log(`Password for user ${username} is incorrect`);
+            }
+        });
     } else {
+        console.log(`User with the username ${username} does not exist`);
         res.sendStatus(httpStatus.BAD_REQUEST);
     }
 });
