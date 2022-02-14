3f4accad59e52afffd82df25741e999128007a3d
ItayAmit <itayamit123@gmail.com>
2022-02-14 14:55:53 +0200

    Error handeling implemented into register page

--- frontend/src/components/pages/registerPage/RegisterPage.jsx
+++ frontend/src/components/pages/registerPage/RegisterPage.jsx
@@ -7,10 +7,78 @@ import './registerPage.css';
 
 export function RegisterPage() {
     const [username, setUsername] = useState('');
+    const [usernameError, setUsernameError] = useState('');
     const [password, setPassword] = useState('');
+    const [passwordError, setPasswordError] = useState('');
+    const [repassword, setRepassword] = useState('');
+    const [repasswordError, setRepasswordError] = useState('');
+    const [firstname, setFirstname] = useState('');
+    const [firstnameError, setFirstnameError] = useState('');
+    const [lastname, setLastname] = useState('');
+    const [lastnameError, setLastnameError] = useState('');
+    const [email, setEmail] = useState('');
+    const [emailError, setEmailError] = useState('');
+
+    const validateEmail = email => {
+        let es = /\S+@\S+\.\S+/;
+        return es.test(email);
+    };
 
     const onRegisterClicked = () => {
-        userApiCommunicator.register(username, password);
+        let flag = true;
+
+        if (!username) {
+            setUsernameError('Username cannot be left empty');
+            flag = false;
+        } else {
+            setUsernameError('');
+        }
+
+        if (password.length < 8) {
+            setPasswordError('Password must contain at least 8 characters');
+            setRepasswordError('');
+            flag = false;
+        } else if (repassword !== password) {
+            setRepasswordError('Confirm password must be the same as password');
+            setPasswordError('');
+            flag = false;
+        } else {
+            setPasswordError('');
+            setRepasswordError('');
+        }
+
+        if (!firstname) {
+            setFirstnameError('First name cannot be left empty');
+            flag = false;
+        } else {
+            setFirstnameError('');
+        }
+
+        if (!lastname) {
+            setLastnameError('Last name cannot be left empty');
+            flag = false;
+        } else {
+            setLastnameError('');
+        }
+
+        if (!email) {
+            setEmailError('Email cannot be left empty');
+            flag = false;
+        } else if (!validateEmail(email)) {
+            setEmailError('Email is not in the correct format');
+            flag = false;
+        } else {
+            setEmailError('');
+        }
+
+        if (flag)
+            userApiCommunicator.register(
+                username,
+                password,
+                firstname,
+                lastname,
+                email
+            );
     };
 
     return (
@@ -24,13 +92,44 @@ export function RegisterPage() {
                     placeholder="Enter username"
                     value={username}
                     onChange={setUsername}
+                    error={usernameError}
                 />
                 <PageInput
                     type="password"
-                    placeholder="Enter password"
                     title="Password"
+                    placeholder="Enter password"
                     value={password}
                     onChange={setPassword}
+                    error={passwordError}
+                />
+                <PageInput
+                    type="password"
+                    title="Confirm Password"
+                    placeholder="Re-enter password"
+                    value={repassword}
+                    onChange={setRepassword}
+                    error={repasswordError}
+                />
+                <PageInput
+                    title="First Name"
+                    placeholder="Enter first name"
+                    value={firstname}
+                    onChange={setFirstname}
+                    error={firstnameError}
+                />
+                <PageInput
+                    title="Last Name"
+                    placeholder="Enter last name"
+                    value={lastname}
+                    onChange={setLastname}
+                    error={lastnameError}
+                />
+                <PageInput
+                    title="Email"
+                    placeholder="Enter email"
+                    value={email}
+                    onChange={setEmail}
+                    error={emailError}
                 />
             </div>
             <div className="register-page-submit">
