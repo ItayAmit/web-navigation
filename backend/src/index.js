3546daba08ff57c4929c33ba46484ae6a2788533
ItayAmit <itayamit123@gmail.com>
2022-02-14 14:53:58 +0200

    Handeling users with the same usernames/emails

--- backend/src/index.js
+++ backend/src/index.js
@@ -16,16 +16,30 @@ app.use(express.json());
 const port = process.env.PORT || 5000;
 
 app.post('/register', (req, res) => {
-    const { username, password } = req.body;
+    const { username, password, firstname, lastname, email } = req.body;
     bcrypt.hash(password, saltRounds, async function (err, hash) {
-        const user = new User({ username, password: hash });
+        const user = new User({
+            username,
+            password: hash,
+            firstname,
+            lastname,
+            email,
+        });
         if (await User.exists({ username })) {
-            console.log(`User with the username ${username} already exists`);
-            res.sendStatus(httpStatus.BAD_REQUEST);
+            res.status(httpStatus.BAD_REQUEST).json({
+                msg: `User with the username ${username} already exists`,
+            });
+        } else if (await User.exists({ email })) {
+            res.status(httpStatus.BAD_REQUEST).json({
+                msg: `The email ${email} is already taken`,
+            });
         } else {
             await user.save();
-            console.log('User has been saved');
-            res.sendStatus(httpStatus.OK);
+            res.status(httpStatus.OK).json({
+                msg: 'User has been saved',
+                redirect: `${user.id}`,
+                user,
+            });
         }
     });
 });
@@ -36,17 +50,25 @@ app.post('/login', async (req, res) => {
         const user = await User.findOne({ username });
         bcrypt.compare(password, user.password, function (err, result) {
             if (result) {
-                console.log(`User ${username} has logged in`);
-                res.sendStatus(httpStatus.OK);
+                res.status(httpStatus.OK).json({
+                    msg: `User ${username} has logged in`,
+                    redirect: `${user.id}`,
+                    user,
+                });
             } else {
-                console.log(`Password for user ${username} is incorrect`);
+                res.status(httpStatus.BAD_REQUEST).json({
+                    msg: `Password for user ${username} is incorrect`,
+                });
             }
         });
     } else {
-        console.log(`User with the username ${username} does not exist`);
-        res.sendStatus(httpStatus.BAD_REQUEST);
+        res.status(httpStatus.BAD_REQUEST).json({
+            msg: `User with the username ${username} does not exist`,
+        });
     }
 });
+
+app.get('/user', async (req, res) => {});
 
 app.listen(port, () => {
     console.log(`Listening on port ${port}`);
