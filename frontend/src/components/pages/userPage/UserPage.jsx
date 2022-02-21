cde2255a99ef6aaca775ccd82d515f8b895261fa
ItayAmit <itayamit123@gmail.com>
2022-02-21 09:01:31 +0200

    Loading user added

--- frontend/src/components/pages/userPage/UserPage.jsx
+++ frontend/src/components/pages/userPage/UserPage.jsx
@@ -1,10 +1,22 @@
+import { useState } from 'react';
+import { useParams } from 'react-router-dom';
+import { userApiCommunicator } from '../../../api/userApiCommunicator';
 import './userPage.css';
 
 export function UserPage() {
+    const { id } = useParams();
+    const [firstname, setFirstname] = useState('Guest');
+    userApiCommunicator.load(id).then(response => {
+        if (response['user']) {
+            setFirstname(response.user.firstname);
+        }
+    });
     return (
         <div className="user-page-container">
             <div className="user-page-header">
-                <span className="user-page-title">Welcome back guest</span>
+                <span className="user-page-title">
+                    Welcome back {firstname}
+                </span>
             </div>
         </div>
     );
