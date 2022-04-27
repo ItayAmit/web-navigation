e86ba5195ff45c029f3a099cc0a8a256304ca2c2
Itay <itayamit13@gmail.com>
2022-04-27 13:39:46 +0300

    Added onChange function

--- frontend/src/components/dropDown/DropDown.jsx
+++ frontend/src/components/dropDown/DropDown.jsx
@@ -1,15 +1,47 @@
+import { useState } from 'react';
+import arrow from '../../images/arrow.png';
+import betterarrow from '../../images/betterarrow.png';
 import './dropDown.css';
 
-export function DropDown({ title = 'DropDown', items }) {
+export function DropDown({ title = 'DropDown', items, onChange }) {
+	const [hidden, setHidden] = useState(true);
+	const [dynamicTitle, setDynamicTitle] = useState(title);
+	const onItemClicked = (value, string) => {
+		onChange(value);
+		setDynamicTitle(string);
+	};
 	return (
 		<div className='drop-down-container'>
-			<span className='drop-down-title'>{title}</span>
-			<ul className='drop-down-list'>
-				{items.map((value, index) => {
-					<li className='drop-down-list-item' key={index}>
-						{value}
-					</li>;
-				})}
+			<div
+				className='drop-down-header'
+				onClick={() => {
+					setHidden(!hidden);
+				}}
+			>
+				<span className='drop-down-header-title'>{dynamicTitle}</span>
+				<img
+					className='drop-down-header-image'
+					src={arrow}
+					style={
+						hidden
+							? { transform: 'rotate(0deg)' }
+							: { transform: 'rotate(180deg)' }
+					}
+				/>
+			</div>
+			<ul className='drop-down-list' hidden={hidden}>
+				{items.map((item, index) => (
+					<li
+						className='drop-down-list-item'
+						key={index}
+						onClick={() => {
+							onItemClicked(item.value, item.string);
+							setHidden(true);
+						}}
+					>
+						{item.string}
+					</li>
+				))}
 			</ul>
 		</div>
 	);
