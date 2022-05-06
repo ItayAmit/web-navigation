f3f923b6252e8bfbae2201ea3a4aa6d7f5056f01
ItayAmit <itayamit123@gmail.com>
2022-05-06 18:10:09 +0300

    Added buttons to navigate through the site

--- frontend/src/components/pages/userPage/UserPage.jsx
+++ frontend/src/components/pages/userPage/UserPage.jsx
@@ -15,8 +15,17 @@ export function UserPage() {
 			if (response['user']) setFirstname(response.user.firstname);
 		});
 	}, []);
+	const onNavigateClicked = () => {
+		navigate('/navigate');
+	};
+	const onBrowseClicked = () => {
+		navigate('/browse');
+	};
 	const onAddSiteClicked = () => {
 		navigate('/addsite');
+	};
+	const onRateSiteClicked = () => {
+		navigate('/rate');
 	};
 	return (
 		<div className='user-page-container'>
@@ -27,9 +36,27 @@ export function UserPage() {
 				<div className='user-page-body-navigation'>
 					<button
 						className='user-page-body-navigation-button'
+						onClick={onNavigateClicked}
+					>
+						Navigate
+					</button>
+					<button
+						className='user-page-body-navigation-button'
+						onClick={onBrowseClicked}
+					>
+						Browse
+					</button>
+					<button
+						className='user-page-body-navigation-button'
 						onClick={onAddSiteClicked}
 					>
 						Add Site
+					</button>
+					<button
+						className='user-page-body-navigation-button'
+						onClick={onRateSiteClicked}
+					>
+						Rate Site
 					</button>
 				</div>
 			</div>
