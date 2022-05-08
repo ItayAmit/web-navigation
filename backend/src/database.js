9edc35cae3a3a2d419cfe4d40b847b783f8fa9b8
ItayAmit <itayamit123@gmail.com>
2022-05-08 15:06:41 +0300

    Added schemas for site characteristics

--- backend/src/database.js
+++ backend/src/database.js
@@ -15,16 +15,69 @@ const siteSchema = new Schema({
 	userid: String,
 	name: String,
 	season: Number,
-	district: Number,
 	difficulty: Number,
 	distance: Number,
 	duration: Number,
+	description: String,
+});
+
+const tripSchema = new Schema({
+	userid: String,
+	date: String,
+	origin: { type: [Number], required: true },
+	destination: { type: [Number], required: true },
+});
+
+const districtSchema = new Schema({
+	key: Number,
+	name: String,
+	north: Number,
+	east: Number,
+	south: Number,
+	west: Number,
+});
+
+const seasonSchema = new Schema({
+	key: Number,
+	name: String,
+});
+
+const difficultySchema = new Schema({
+	key: Number,
+	name: String,
+});
+
+const distanceSchema = new Schema({
+	key: Number,
+	name: String,
+});
+
+const durationSchema = new Schema({
+	key: Number,
+	name: String,
+});
+
+const typeSchema = new Schema({
+	key: Number,
+	name: String,
 });
 
 const User = mongoose.model('User', userSchema);
 const Site = mongoose.model('Site', siteSchema);
+const District = mongoose.model('District', districtSchema);
+const Season = mongoose.model('Season', seasonSchema);
+const Difficulty = mongoose.model('Difficulty', difficultySchema);
+const Distance = mongoose.model('Distance', distanceSchema);
+const Duration = mongoose.model('Duration', durationSchema);
+const Type = mongoose.model('Type', typeSchema);
 
 module.exports = {
 	User,
 	Site,
+	District,
+	Season,
+	Difficulty,
+	Distance,
+	Duration,
+	Type,
 };
