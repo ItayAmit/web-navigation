c5b5429c1f4b3f1945e5f9f694e759402549f63f
ItayAmit <itayamit123@gmail.com>
2022-05-09 21:12:40 +0300

    Added get request function with params

--- frontend/src/api/apiCommunicator.js
+++ frontend/src/api/apiCommunicator.js
@@ -2,6 +2,7 @@ import Axios from 'axios';
 
 export const apiCommunicator = {
 	get,
+	getWithParams,
 	post,
 };
 
@@ -9,6 +10,16 @@ const API_URL = 'http://localhost:5000';
 
 async function get(path) {
 	return await Axios.get(`${API_URL}/${path}`)
+		.then(response => {
+			return response.data;
+		})
+		.catch(error => {
+			return { msg: error.response.data.msg };
+		});
+}
+
+async function getWithParams(path, params) {
+	return await Axios.get(`${API_URL}/${path}`, { params })
 		.then(response => {
 			return response.data;
 		})
