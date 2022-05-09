8723a899101e76b7eb55c08a33e317a9a2da72ef
ItayAmit <itayamit123@gmail.com>
2022-05-09 21:14:09 +0300

    Added site card for to display sites when browsing

---
+++ frontend/src/components/siteCard/SiteCard.jsx
@@ -0,0 +1,56 @@
+import './siteCard.css';
+
+export function SiteCard({
+	site,
+	onClick,
+	number,
+	highlighted,
+	districts,
+	seasons,
+	difficulties,
+	distances,
+	durations,
+	types,
+}) {
+	const onCardClicked = () => {
+		onClick(site._id, number);
+	};
+	return (
+		<div
+			className='site-card-container'
+			onClick={onCardClicked}
+			style={
+				highlighted ? { borderColor: '#add8e6' } : { borderColor: '666666' }
+			}
+		>
+			<span className='site-card-title'>{site.name}</span>
+			<div className='site-card-row'>
+				{/* <span className='site-card-characteristic'>
+					District: {districts[site.district].name}
+				</span> */}
+				<span className='site-card-characteristic'>
+					Season: {seasons[site.season].name}
+				</span>
+				<span className='site-card-characteristic'>
+					Difficulty: {difficulties[site.difficulty].name}
+				</span>
+			</div>
+			<div className='site-card-row'>
+				<span className='site-card-characteristic'>
+					Distance: {distances[site.distance].name}
+				</span>
+				<span className='site-card-characteristic'>
+					Duration: {durations[site.duration].name}
+				</span>
+				<span className='site-card-characteristic'>
+					Type: {types[site.type].name}
+				</span>
+			</div>
+			<div className='site-card-row'>
+				<span className='site-card-characteristic'>
+					Description: {site.description}
+				</span>
+			</div>
+		</div>
+	);
+}
